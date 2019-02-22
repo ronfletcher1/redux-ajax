edc3df435a342bc2dc3149c965260b4f8fc59008
ronfletcher1 <ronfletcher1@me.com>
2019-02-21 19:31:42 -0500

    rons weather/stock update first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import SearchBar from './containers/SearchBar';
+import Weather from './containers/Weather';
+import Stocks from './containers/Stocks';
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        
+        <SearchBar />
+        <div className="col-sm-6">
+        <Weather />
+        </div>
+        <div className="col-sm-6">
+        <Stocks />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Stocks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Stocks extends Component{
+    render(){
+        return(
+            <h1>Stock Component!!</h1>
+        )    
+    }
+}
+
+export default Stocks;
